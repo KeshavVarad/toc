ea3e26fffe732925efe2d32f350ce314a191555c
Keshav Varadarajan <kvthevk@gmail.com>
2020-05-23 22:22:50 -0400

    The pagination and cache update

--- app.js
+++ app.js
@@ -1,42 +1,72 @@
-const express = require('express');
-const axios = require('axios');
-const path = require('path');
-const dateTime = require('date-and-time');
-const fs = require('fs');
+const express = require("express");
+const axios = require("axios");
+const path = require("path");
+const dateTime = require("date-and-time");
+const fs = require("fs");
+const NodeCache = require("node-cache");
+const myCache = new NodeCache({
+    stdTTL: 3600,
+    checkperiod: 120
+});
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (request, response) => {
-    response.redirect('home');
-})
+app.get("/", (request, response) => {
+    response.redirect("home");
+});
 
-app.get('/home', async (request, response) => {
-    const states_query = await axios.get('https://covidtracking.com/api/states');
-    const us_query = await axios.get('http://covidtracking.com/api/us');
-    const stateNamesRaw = fs.readFileSync(path.resolve(__dirname, "./public/data/state_names.json"));
-    const stateNames = JSON.parse(stateNamesRaw);    
-    var statesData = states_query.data;
-    var usData = us_query.data;
+app.get("/home", async (request, response) => {
+    var states_query = [];
+    var us_query = [];
+    var statesData = [];
+    var usData = [];
+
+    var stateValue = myCache.get("stateData");
+    if (stateValue === undefined) {
+        states_query = await axios.get("https://covidtracking.com/api/states");
+        myCache.set("stateData", states_query.data, 3600);
+        statesData = states_query.data;
+        
+    }
+    else {
+        statesData = stateValue;
+    }
+
+    var usValue = myCache.get("usData");
+    console.log("US Value: ", usValue);
+    if (usValue == undefined) {
+        us_query = await axios.get("http://covidtracking.com/api/us");
+        myCache.set("usData", us_query.data, 3600);
+        usData = us_query.data;
+    }
+    else {
+        usData = usValue;
+    }
+
+    const stateNamesRaw = fs.readFileSync(
+        path.resolve(__dirname, "./public/data/state_names.json")
+    );
+    const stateNames = JSON.parse(stateNamesRaw);
     var dates = [];
-    statesData.map(state => {
+    statesData.map((state) => {
         var dateChecked = state.dateChecked;
         var date = new Date(dateChecked);
         const pattern = dateTime.compile("MM/DD/YY hh:mm");
         var dateString = dateTime.format(date, pattern);
         state.dateChecked = dateString;
-        state.stateName = stateNames[state.state];        
-    })
-    usData.map(country => {
+        state.stateName = stateNames[state.state];
+    });
+    usData.map((country) => {
         var dateChecked = country.lastModified;
         var date = new Date(dateChecked);
         const pattern = dateTime.compile("MM/DD/YY hh:mm");
         var dateString = dateTime.format(date, pattern);
-        country.lastModified = dateString;        
-    })
+        country.lastModified = dateString;
+    });
     // for(state in statesData) {
     //     var dateChecked = state.dateChecked;
     //     var date = new Date(dateChecked);
@@ -45,28 +75,30 @@ app.get('/home', async (request, response) => {
     //     state.dateChecked = dateString;
     // };
 
-    response.render('home', {
-        us: us_query.data,
-        states: statesData
+    response.render("home", {
+        us: usData,
+        states: statesData,
     });
-})
+});
 
-app.get('/news', async (request, response) => {
-    var feedJSON = await axios.get('https://tools.cdc.gov/podcasts/feed.asp?feedid=183&format=json');
-        response.render('news', {
-            data: feedJSON.data.entries
-        })
-})
+app.get("/news", async (request, response) => {
+    var feedJSON = await axios.get(
+        "https://tools.cdc.gov/podcasts/feed.asp?feedid=183&format=json"
+    );
+    response.render("news", {
+        data: feedJSON.data.entries,
+    });
+});
 
-app.get('/graphs', async (request, response) => {
-    var usDailyJSON = await axios.get('https://covidtracking.com/api/us/daily');
+app.get("/graphs", async (request, response) => {
+    var usDailyJSON = await axios.get("https://covidtracking.com/api/us/daily");
     var usDailyJSONData = usDailyJSON.data;
     var dates = [];
     var positives = [];
     var numDays = usDailyJSONData.length;
-    for(var i=numDays-1; i>0; i--) {
+    for (var i = numDays - 1; i > 0; i--) {
         var date = usDailyJSONData[i].date.toString();
-        var year = date.substring(0,4);
+        var year = date.substring(0, 4);
         var month = date.substring(4, 6);
         var day = date.substring(6, 8);
         date = new Date(year, month, day);
@@ -76,39 +108,37 @@ app.get('/graphs', async (request, response) => {
         var numPositive = usDailyJSONData[i].positive;
         dates.push(dateString);
         positives.push(numPositive);
-    };
-
+    }
 
     var chartData = {
-        type: 'line',
+        type: "line",
         data: {
             labels: dates,
             datasets: [
                 {
                     data: positives,
-                    label: "US Positive Test Results"
-                }
-            ]
+                    label: "US Positive Test Results",
+                },
+            ],
         },
         options: {
-            responsive: true
-        }
-    }
+            responsive: true,
+        },
+    };
 
-    response.render('graphs', {
+    response.render("graphs", {
         data: JSON.stringify(chartData),
     });
-})
-
-app.get('/about', (request, response) => {
-    response.render('about');
 });
 
-app.get('/donations', (request, response) => {
-    response.render('donations');
+app.get("/about", (request, response) => {
+    response.render("about");
+});
+
+app.get("/donations", (request, response) => {
+    response.render("donations");
 });
 
 app.listen(8080, () => {
-    console.log("Listening on 8080")
+    console.log("Listening on 8080");
 });
-
---
+++ public/scripts/pagination.js
@@ -0,0 +1,7 @@
+// $(document).ready(function () {
+//     $('#statesTable').DataTable();
+//     $('.dataTables_length').addClass('bs-select');
+// });
+$(document).ready( function () {
+    $('#statesTable').DataTable();
+} );
