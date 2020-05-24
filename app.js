0e8d059e79e05cb129f4d3bf91f08683241a745f
Keshav Varadarajan <kvthevk@gmail.com>
2020-05-23 22:50:20 -0400

    The state name update

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 const NodeCache = require("node-cache");
 const myCache = new NodeCache({
     stdTTL: 3600,
-    checkperiod: 120
+    checkperiod: 3650
 });
 
 const app = express();
